34eb44bebb8ec3cd99e5c2de106dc65532f11fc6
Vipul Bokde <vipulbokde1919@gmail.com>
2025-02-03 13:32:33 +0530

    selenium java maven framework creation

---
+++ Selenium_Java/SeleniumTestNGFramework/src/main/java/org/java_selenium_framework/SeleniumTestNGFramework/App.java
@@ -0,0 +1,10 @@
+package org.java_selenium_framework.SeleniumTestNGFramework;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
